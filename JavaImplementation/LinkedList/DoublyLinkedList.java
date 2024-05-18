61a823ada9ccebe7e8dcd577f370bfd046dbf7b3
Lucas Leow <leowjianhoong@gmail.com>
2024-05-18 18:46:48 +0800

    Complete removeLast for DoublyLL

--- JavaImplementation/LinkedList/DoublyLinkedList.java
+++ JavaImplementation/LinkedList/DoublyLinkedList.java
@@ -17,6 +17,35 @@ public class DoublyLinkedList {
         this.tail = newNode;
         this.length = 1;
     }
+
+    public void append(int val) {
+        Node newNode = new Node(val);
+        if (this.length == 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+    }
+
+    public Node removeLast() {
+        if (this.length == 0) return null;
+        Node temp = this.tail;
+        this.tail = temp.prev;
+        this.tail.next = null;
+        temp.prev = null;
+        this.length--;
+
+        if (this.length == 0) { // if LL only has single element
+            this.head = null;
+            this.tail = null;
+        }
+        return temp;
+    }
+
     public void printList() {
         Node cur = head;
         while (cur != null) {
