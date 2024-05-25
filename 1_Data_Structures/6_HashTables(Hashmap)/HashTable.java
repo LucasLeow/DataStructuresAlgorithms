5d000949f7c4493e6e2028e93bc76ec229dbb64c
Lucas Leow <leowjianhoong@gmail.com>
2024-05-25 20:12:06 +0800

    Complete HashTable set method

--- 1_Data_Structures/6_HashTables(Hashmap)/HashTable.java
+++ 1_Data_Structures/6_HashTables(Hashmap)/HashTable.java
@@ -16,4 +16,46 @@ public class HashTable {
     public HashTable() {
         this.dataMap = new Node[this.size];
     }
+
+    private int hashFunction(String key) {
+        int hash = 0;
+
+        char[] keyChars = key.toCharArray();
+
+        for (int i = 0; i < keyChars.length; i++) {
+            int asciiValue = keyChars[i]; // saving char as int will convert it to ascii equivalent value
+            hash = (hash + asciiValue * 23) % dataMap.length; // 23 is random prime no., resulting in more random number
+            // % dataMap.length will give range between 0 and (length of Array - 1)
+        }
+        return hash;
+    }
+
+    public void set(String key, int val) {
+        Node newNode = new Node(key, val);
+        int idx = this.hashFunction(key);
+
+        if (this.dataMap[idx] == null) {
+            this.dataMap[idx] = newNode;
+        } else {
+            Node temp = this.dataMap[idx];
+
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+            newNode.prev = temp;
+        }
+
+    }
+
+    public void printTable() {
+        for (int i = 0; i < dataMap.length; i++) {
+            System.out.println(i + " : ");
+            Node temp = dataMap[i];
+            while (temp != null) {
+                System.out.println("{" + temp.key + ":" + temp.value + "} ");
+                temp = temp.next;
+            }
+        }
+    }
 }
