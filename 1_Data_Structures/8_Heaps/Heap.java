6315319f02483dbc7145410848dac4e83e83ff13
Lucas Leow <leowjianhoong@gmail.com>
2024-05-26 18:25:30 +0800

    Complete Heap Insertion method

--- 1_Data_Structures/8_Heaps/Heap.java
+++ 1_Data_Structures/8_Heaps/Heap.java
@@ -8,15 +8,15 @@ public class Heap {
         this.heap = new ArrayList<>();
     }
 
-    private int leftChild(int parentIdx) {
-        return 2 * parentIdx;
+    private int getLeftChildIdx(int parentIdx) {
+        return 2 * parentIdx + 1;
     }
 
-    private int rightChild(int parentIdx) {
+    private int getRightChildIdx(int parentIdx) {
         return 2 * parentIdx + 2;
     }
 
-    private int parentIdx(int childIdx) {
+    private int getParentIdx(int childIdx) {
         return (childIdx - 1) / 2;
     }
 
@@ -27,8 +27,28 @@ public class Heap {
         this.heap.set(idx2, temp);
     }
 
+    public void insert(int value) {
+        this.heap.add(value);
+        int curIdx = this.heap.size() - 1;
+
+        while (curIdx > 0 && this.heap.get(curIdx) > this.heap.get(getParentIdx(curIdx))) {
+            this.swap(curIdx, this.getParentIdx(curIdx));
+            curIdx = this.getParentIdx((curIdx));
+        }
+    }
+
     public List<Integer> getHeap() { // return copy of existing heap
         return new ArrayList<>(heap);
+    }
+
+    public static void main(String[] args) {
+        Heap myHeap = new Heap();
+        myHeap.insert(99);
+        myHeap.insert(26);
+        myHeap.insert(34);
+        myHeap.insert(67);
+
+        System.out.println(myHeap.getHeap());
     }
 
 }
