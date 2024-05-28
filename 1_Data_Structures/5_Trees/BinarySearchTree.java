2ca9c43bc061e155dc17a1602ff736124c4bad5a
Lucas Leow <leowjianhoong@gmail.com>
2024-05-28 22:04:05 +0800

    Implement recursive contains method for BST

--- 1_Data_Structures/5_Trees/BinarySearchTree.java
+++ 1_Data_Structures/5_Trees/BinarySearchTree.java
@@ -56,4 +56,22 @@ public class BinarySearchTree {
         }
         return false;
     }
+
+    // actual contains method using recursion (not exposed as API)
+    private boolean rContains(Node currentNode, int value) {
+
+        if (currentNode == null) return false; // tree is initially empty or value does not exist in BST
+        if (currentNode.value == value) return true; // base case
+
+        if (value < currentNode.value) { // value lower than root, traverse left sub-tree
+            return this.rContains(currentNode.left, value);
+        } else { // value greater than root, traverse right sub-tree
+            return this.rContains(currentNode.right, value); 
+        }
+    }
+
+    // recursive contains method exposed as API for others to use (method overload)
+    public boolean rContains(int value) {
+        return rContains(this.root, value);
+    }
  }
