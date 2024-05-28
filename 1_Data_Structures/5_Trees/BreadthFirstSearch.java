9a4f2524459204a27933163571f07f662fbdf653
Lucas Leow <leowjianhoong@gmail.com>
2024-05-29 00:16:28 +0800

    Complete BFS traversal logic

---
+++ 1_Data_Structures/5_Trees/BreadthFirstSearch.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BreadthFirstSearch {
+    Node root;
+    class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int val) {
+            this.value = val;
+        }
+    }
+    public ArrayList<Integer> BFS() {
+
+        Node currentNode = root;
+        Queue<Node> queue = new LinkedList<>();
+        ArrayList<Integer> results = new ArrayList<>();
+
+        queue.add(currentNode);
+        while (queue.size() > 0) {
+            currentNode = queue.remove();
+            results.add(currentNode.value);
+            if (currentNode.left != null) {
+                queue.add(currentNode.left);
+            }
+            if (currentNode.right != null) {
+                queue.add(currentNode.right);
+            }
+        }
+        return results;
+    }
+}
