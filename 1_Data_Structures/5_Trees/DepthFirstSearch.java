2e1e4f60068f4729ed9e7123f7ceb7143ee62f73
LucasLeow <leowjianhoong@gmail.com>
2024-05-29 10:38:53 +0800

    Complete DFS Pre-order & Post-order

---
+++ 1_Data_Structures/5_Trees/DepthFirstSearch.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class DepthFirstSearch {
+    Node root;
+    class Node {
+        int value;
+        Node left;
+        Node right;
+        Node(int val) {
+            this.value = val;
+        }
+    }
+    public ArrayList<Integer> DFSPreOrder() {
+        ArrayList<Integer> results = new ArrayList<>();
+        class Traverse { // create class so can nest method within method in java
+            public Traverse(Node currentNode) {
+                results.add(currentNode.value);
+                if (currentNode.left != null) {
+                    new Traverse(currentNode.left); // need new keyword since its constructor
+                }
+                if (currentNode.right != null) {
+                    new Traverse(currentNode.right);
+                }
+            }
+        }
+        new Traverse(this.root);
+        return results;
+    }
+
+    public ArrayList<Integer> DFSPostOrder() {
+        ArrayList<Integer> results = new ArrayList<>();
+        class Traverse {
+            public Traverse(Node currentNode) {
+                if (currentNode.left != null) {
+                    new Traverse(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    new Traverse(currentNode.right);
+                }
+                results.add(currentNode.value);
+            }
+        }
+        new Traverse(this.root);
+        return results;
+    }
+}
